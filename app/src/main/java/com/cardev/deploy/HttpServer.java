6c67264986e6f6b635357e3d286dbe3b7d8256ad
fengyunfei888-blip <fengyunfei888@gmail.com>
2026-09-02 00:34:42 +0800

    Implement getHomePage method in HttpServer
    Add getHomePage method to return HTML for the home page.

--- app/src/main/java/com/cardev/deploy/HttpServer.java
+++ app/src/main/java/com/cardev/deploy/HttpServer.java
@@ -35,4 +35,36 @@ public class HttpServer {
     }
 
 
+    public String getHomePage(){
+
+
+        return
+
+        "<html>" +
+
+        "<head>" +
+
+        "<meta charset='utf-8'>" +
+
+        "<title>车机应用部署助手</title>" +
+
+        "</head>" +
+
+
+        "<body>" +
+
+        "<h2>车机应用部署助手</h2>" +
+
+        "<p>发现安装包</p>" +
+
+        "<button>下载安装</button>" +
+
+        "</body>" +
+
+
+        "</html>";
+
+    }
+
+
 }
